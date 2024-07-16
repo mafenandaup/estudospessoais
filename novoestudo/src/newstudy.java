c49a24a0a33b3618b99eacb518b9b96c8392c97c
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-07-16 18:04:50 -0300

    oii

--- novoestudo/src/newstudy.java
+++ novoestudo/src/newstudy.java
@@ -1,3 +1,14 @@
-public class newstudy {
+import java.util.Scanner;
 
+public class newstudy {
+    public static void main(String[] args) {
+        double num;
+        System.out.println("digite um número qualquer pra encontrar sua raiz quadrada");
+        Scanner sc = new Scanner(System.in);
+        sqrt(num = sc.nextDouble());
+    }
+
+    public static void sqrt(double x) {
+
+    }
 }
