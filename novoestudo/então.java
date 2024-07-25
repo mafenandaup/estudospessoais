34966ec2e93cc08ff362b2798cdb1e1abc22483e
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-07-25 11:29:30 -0300

    ai

--- novoestudo/então.java
+++ novoestudo/então.java
@@ -2,7 +2,16 @@
 
 public class então {
     
-    public static void main(String[] args) {
-        System.out.println("oi");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Olá, sou o Simpático Automático");
+       Thread.sleep(1000);
+       System.out.println("Preciso tirar uma dúvida");
+       Thread.sleep(1300);
+       System.out.println("...");
+       Thread.sleep(2500);
+       System.out.println("Qual seu nome, afinal?");
+    }
+    public static void reps(int i){
+for 
     }
 }
