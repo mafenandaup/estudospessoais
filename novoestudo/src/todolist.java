de20907848cf3952a11d94dc162c4620a0e9e4b2
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-07-25 18:21:43 -0300

    oi

--- novoestudo/src/todolist.java
+++ novoestudo/src/todolist.java
@@ -13,15 +13,23 @@ public class todolist {
         inicio();
     }
 
-    public static void inicio() throws InterruptedException{
+    public static void inicio() throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
         LocalDate data = LocalDate.now();
-		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		String datanova = dateFormat.format(data);
-      System.out.println("*TODO-LIST: " +datanova+ "*");
-      Thread.sleep(400);
-      System.out.println("gostaria de adicionar uma tarefa? (Y/n)");
-   
-    
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String datanova = dateFormat.format(data);
+        System.out.println("*TODO-LIST: " + datanova + "*");
+        Thread.sleep(400);
+        System.out.println("--------------");
+    }
+
+    public static void newtask (){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("gostaria de adicionar uma tarefa? (Y/n)");
+        String answer = sc.nextLine();
+        if (answer.equalsIgnoreCase("Y")){
+          System.out.println("digite a tarefa que gostaria de inserir");
+          String tarefa = sc.nextLine();
+    }
    
     }
-}
